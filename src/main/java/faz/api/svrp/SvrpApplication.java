d7318b0450b9d40867d5b1ef34dfd9a775c14afe
FazGit2211 <zuletafacundoadolfo2211@gmail.com>
2025-11-18 15:38:03 -0300

    Inicio del proyecto.

---
+++ src/main/java/faz/api/svrp/SvrpApplication.java
@@ -0,0 +1,13 @@
+package faz.api.svrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvrpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvrpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/faz/api/svrp/SvrpApplicationTests.java
@@ -0,0 +1,13 @@
+package faz.api.svrp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvrpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
